86666f61fc49835b0989bfacedd883ae115d963c
Joseph <sts-joseph.callao@rakuten.com>
2023-07-12 13:33:53 +0800

    added edit and delete functionality

--- FullStack.API/FullStack.API/Controllers/EmployeesController.cs
+++ FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -48,5 +48,44 @@ namespace FullStack.API.Controllers
 
             return Ok(employee);
         }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee employeeRequest)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = employeeRequest.Name;
+            employee.Email = employeeRequest.Email;
+            employee.Phone = employeeRequest.Phone;
+            employee.Salary = employeeRequest.Salary;
+            employee.Department = employeeRequest.Department;
+
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _fullStackDbContext.Employees.Remove(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
     }
 }
